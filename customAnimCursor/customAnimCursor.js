7d765233661491bfab757875ced63b029ef13868
SDen4 <asw325@yandex.ru>
2020-11-22 22:48:57 +0500

    custom animated cursor

---
+++ customAnimCursor/customAnimCursor.js
@@ -0,0 +1,13 @@
+const cursorDot = document.querySelector('.customAnimCursor');
+let clientX;
+let clientY;
+
+//mouse coords
+document.addEventListener("mousemove", e => {
+    clientX = e.clientX;
+    clientY = e.clientY;
+    console.log(clientX);
+    console.log(clientY);
+
+    cursorDot.style.transform = `translate(${clientX}px, ${clientY}px)`;
+});
