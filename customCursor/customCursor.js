d91283428acc09eadf988e4debe187621b4dbdd6
SDen4 <asw325@yandex.ru>
2020-11-23 21:23:09 +0500

    clean

--- customCursor/customCursor.js
+++ customCursor/customCursor.js
@@ -6,8 +6,5 @@ let clientY;
 document.addEventListener("mousemove", e => {
     clientX = e.clientX;
     clientY = e.clientY;
-    console.log(clientX);
-    console.log(clientY);
-
     cursorDot.style.transform = `translate(${clientX}px, ${clientY}px)`;
 });
